ba1e20f2884b7a45d08406aa74cddd5e6101e09f
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-04 03:46:04 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,5 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Egg Chronicles", 1, 2), (2, "Forbidden Realm", 2, 1);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Egg Chronicles", 1983, 1), (2, "Egg Chronicles: The Awakening", 1992, 1), (3, "Egg Chronicles: Revelations", 2001, 1), (4, "Forbidden Realm", 2003, 2), (5, "2 Forbidden 2 Realm", 2002, 2), (6, "Forbidd3n R3alm III", 2005, 2);
+INSERT INTO subgenres (id, name) VALUES (1, "Medieval"), (2, "Culinary");
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
+()
