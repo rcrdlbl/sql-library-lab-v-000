1399f8fb2e9daa05256628dbe50e9c63857ac334
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-04 03:11:24 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Egg Chronicles", 1, 2), (2, "Forbidden Realm", 2, 1);
--- lib/schema.sql
+++ lib/schema.sql
@@ -1,29 +1,29 @@
 CREATE TABLE series (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
   subgenre_id INTEGER
 );
 
 CREATE TABLE subgenres (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   name TEXT
 );
 
 CREATE TABLE authors (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   name TEXT
 );
 
 CREATE TABLE books (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
-  series_id INTEGER,
+  series_id INTEGER
 );
 
 CREATE TABLE characters (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   name TEXT,
   motto TEXT,
   species TEXT,
@@ -32,7 +32,7 @@ CREATE TABLE characters (
 );
 
 CREATE TABLE character_books (
-  id PRIMARY KEY INTEGER,
+  id INTEGER PRIMARY KEY,
   character_id INTEGER,
   book_id INTEGER
-)
+);
