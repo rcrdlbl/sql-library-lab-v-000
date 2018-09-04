4b1172bd79630ba73e7afb4ef9f4bd6bc5f8752c
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-04 02:20:07 +0000

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE series (
+  id PRIMARY KEY INTEGER,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
+);
+
+CREATE TABLE subgenres (
+  id PRIMARY KEY INTEGER,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id PRIMARY KEY INTEGER,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id PRIMARY KEY INTEGER,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER,
+);
+
+CREATE TABLE characters (
+  id PRIMARY KEY INTEGER,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE character_books (
+  id PRIMARY KEY INTEGER,
+  character_id INTEGER,
+  book_id INTEGER
+)
